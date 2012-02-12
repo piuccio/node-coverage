288121c586b80782284830862f94d751bc7122ca
Fabio Crisci <piuccio@gmail.com>
2012-02-12 15:09:48 +0100

    better JSON structure for reports, fixes #2 Administrative views are broken!

--- test/executeCode.js
+++ test/executeCode.js
@@ -28,40 +28,22 @@ function compare (file, code, options) {
 	var json = JSON.parse(serialized);
 
 	var generatedReport = report.generateAll(json);
-	var globalResult = generatedReport.global;
-	var result = generatedReport.files[file];
 
 	var expected = expectedCoverage[file.substring(file.lastIndexOf("/") + 1)];
 	if (options && options["condition"] === false) {
 		expected.conditions = 0;
 		expected.conditionsTrue = 0;
 		expected.conditionsFalse = 0;
+		expected.conditionsPercentage = 100;
 	}
 	if (options && options["function"] === false) {
 		expected.functions = 0;
 		expected.functionsCalled = 0;
+		expected.functionsPercentage = 100;
 	}
 
-	testObject.equal(result.total, expected.total, "total " + file);
-	testObject.equal(result.visited, expected.visited, "visited " + file);
-
-	testObject.equal(result.conditions, expected.conditions, "conditions " + file);
-	testObject.equal(result.conditionsTrue, expected.conditionsTrue, "conditionsTrue " + file);
-	testObject.equal(result.conditionsFalse, expected.conditionsFalse, "conditionsFalse " + file);
-
-	testObject.equal(result.functions, expected.functions, "functions " + file);
-	testObject.equal(result.functionsCalled, expected.functionsCalled, "functionsCalled " + file);
-
-	// Since we test one file at the time, global results should be equal
-	testObject.equal(globalResult.total, expected.total, "global total " + file);
-	testObject.equal(globalResult.visited, expected.visited, "global visited " + file);
-
-	testObject.equal(globalResult.conditions, expected.conditions, "global conditions " + file);
-	testObject.equal(globalResult.conditionsTrue, expected.conditionsTrue, "global conditionsTrue " + file);
-	testObject.equal(globalResult.conditionsFalse, expected.conditionsFalse, "global conditionsFalse " + file);
-
-	testObject.equal(globalResult.functions, expected.functions, "global functions " + file);
-	testObject.equal(globalResult.functionsCalled, expected.functionsCalled, "global functionsCalled " + file);
+	assertCoverageEquals(generatedReport.files[file], expected, file);
+	assertCoverageEquals(generatedReport.global, expected, file);
 
 	waitingFiles -= 1;
 	if (waitingFiles < 1) {
@@ -69,11 +51,33 @@ function compare (file, code, options) {
 	}
 };
 
+function assertCoverageEquals (measured, expected, file) {
+	var statementCoverage = measured.statements;
+	testObject.equal(statementCoverage.total, expected.total, "total statements " + file);
+	testObject.equal(statementCoverage.covered, expected.visited, "covered statements " + file);
+	// being float we compare to 1E-5
+	testObject.equal(statementCoverage.percentage.toFixed(5), 
+		expected.statementsPercentage.toFixed(5), "percentage statements " + file);
+
+	var conditionCoverage = measured.conditions;
+	testObject.equal(conditionCoverage.total, expected.conditions, "conditions " + file);
+	testObject.equal(conditionCoverage.coveredTrue, expected.conditionsTrue, "conditionsTrue " + file);
+	testObject.equal(conditionCoverage.coveredFalse, expected.conditionsFalse, "conditionsFalse " + file);
+	testObject.equal(conditionCoverage.percentage.toFixed(5), 
+		expected.conditionsPercentage.toFixed(5), "percentage conditions " + file);
+
+	var functionCoverage = measured.functions;
+	testObject.equal(functionCoverage.total, expected.functions, "functions " + file);
+	testObject.equal(functionCoverage.covered, expected.functionsCalled, "functionsCalled " + file);
+	testObject.equal(functionCoverage.percentage.toFixed(5), 
+		expected.functionsPercentage.toFixed(5), "percentage conditions " + file);
+};
+
 function compareWithOptions (options) {
 	return function (file, code) {
 		compare(file, code, options);
 	};
-}
+};
 
 var testObject;
 var waitingFiles;
--- test/results/code.js
+++ test/results/code.js
@@ -2,127 +2,169 @@ exports.results = {
 	"base.js" : {
 		total : 3,
 		visited : 3,
+		statementsPercentage : 100,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 0,
-		functionsCalled : 0
+		functionsCalled : 0,
+		functionsPercentage : 100
 	},
 	"longlines.js" : {
 		total : 1,
 		visited : 1,
+		statementsPercentage : 100,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 0,
-		functionsCalled : 0
+		functionsCalled : 0,
+		functionsPercentage : 100
 	},
 	"function.js" : {
 		total : 4,
 		visited : 4,
+		statementsPercentage : 100,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 1,
-		functionsCalled : 1
+		functionsCalled : 1,
+		functionsPercentage : 100
 	},
 	"if_no_block.js" : {
 		total : 9,
 		visited : 6,
+		statementsPercentage : 100 * 6 / 9,
 		conditions : 2,
 		conditionsTrue : 1,
 		conditionsFalse : 1,
+		conditionsPercentage : 50,
 		functions : 2,
-		functionsCalled : 1
+		functionsCalled : 1,
+		functionsPercentage : 50
 	},
 	"no_block.js" : {
 		total : 8,
 		visited : 5,
+		statementsPercentage : 100 * 5 / 8,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 0,
-		functionsCalled : 0
+		functionsCalled : 0,
+		functionsPercentage : 100
 	},
 	"label_continue.js" : {
 		total : 2,
 		visited : 2,
+		statementsPercentage : 100,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 0,
-		functionsCalled : 0
+		functionsCalled : 0,
+		functionsPercentage : 100
 	},
 	"cond_simple_if.js" : {
 		total : 22,
 		visited : 22,
+		statementsPercentage : 100,
 		conditions : 9,
 		conditionsTrue : 9,
 		conditionsFalse : 0,
+		conditionsPercentage : 50,
 		functions : 3,
-		functionsCalled : 3
+		functionsCalled : 3,
+		functionsPercentage : 100
 	},
 	"cond_simple_if_false.js" : {
 		total : 24,
 		visited : 15,
+		statementsPercentage : 100 * 15 / 24,
 		conditions : 10,
 		conditionsTrue : 1,
 		conditionsFalse : 9,
+		conditionsPercentage : 50,
 		functions : 3,
-		functionsCalled : 3
+		functionsCalled : 3,
+		functionsPercentage : 100
 	},
 	"function_object.js" : {
 		total : 7,
 		visited : 7,
+		statementsPercentage : 100,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 4,
-		functionsCalled : 4
+		functionsCalled : 4,
+		functionsPercentage : 100
 	},
 	"cond_decision_if.js" : {
 		total : 17,
 		visited : 15,
+		statementsPercentage : 100 * 15 / 17,
 		conditions : 7,
 		conditionsTrue : 5,
 		conditionsFalse : 2,
+		conditionsPercentage : 50,
 		functions : 1,
-		functionsCalled : 1
+		functionsCalled : 1,
+		functionsPercentage : 100
 	},
 	"cond_multiple_if.js" : {
 		total : 14,
 		visited : 11,
+		statementsPercentage : 100 * 11 / 14,
 		conditions : 14,
 		conditionsTrue : 7,
 		conditionsFalse : 5,
+		conditionsPercentage : 50 * (7 + 5) / 14,
 		functions : 1,
-		functionsCalled : 1
+		functionsCalled : 1,
+		functionsPercentage : 100
 	},
 	"cond_group_if.js" : {
 		total : 4,
 		visited : 2,
+		statementsPercentage : 50,
 		conditions : 7,
 		conditionsTrue : 3,
 		conditionsFalse : 2,
+		conditionsPercentage : 50 * (3 + 2) / 7,
 		functions : 0,
-		functionsCalled : 0
+		functionsCalled : 0,
+		functionsPercentage : 100
 	},
 	"if_else.js" : {
 		total : 10,
 		visited : 7,
+		statementsPercentage : 100 * 7 / 10,
 		conditions : 6,
 		conditionsTrue : 4,
 		conditionsFalse : 1,
+		conditionsPercentage : 50 * (4 + 1) / 6,
 		functions : 1,
-		functionsCalled : 1
+		functionsCalled : 1,
+		functionsPercentage : 100
 	},
 	"empty_function.js" : {
 		total : 7,
 		visited : 7,
+		statementsPercentage : 100,
 		conditions : 0,
 		conditionsTrue : 0,
 		conditionsFalse : 0,
+		conditionsPercentage : 100,
 		functions : 4,
-		functionsCalled : 3
+		functionsCalled : 3,
+		functionsPercentage : 100 * 3 / 4
 	}
 };
